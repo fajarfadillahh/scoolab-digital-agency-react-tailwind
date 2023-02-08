130f32ef830c8b380331e40956de22e42f54fe17
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-08 19:24:39 +0700

    modified: tracked file TestimonialCard

---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { RiStarFill } from "react-icons/ri";
+
+// import icon
+import CommaIcon from "../assets/images/icons/icon-comma.png";
+
+const TestimonialCard = ({ comment, profileImg, name, profession, rating }) => {
+  return (
+    <div className="grid gap-8">
+      <div>
+        <img src={CommaIcon} alt="comma icon" className="w-10 pb-2" />
+        <p className="section-text">{comment}</p>
+      </div>
+
+      <div>
+        <img src={profileImg} alt="profile img" className="w-16 pb-4" />
+        <h3 className="section-title pb-1 text-[28px]">{name}</h3>
+        <p className="section-text pb-4">{profession}</p>
+        <div className="inline-flex items-center gap-1 text-[1.3rem] text-yellow-400">
+          {rating}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;

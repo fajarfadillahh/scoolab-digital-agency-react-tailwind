bef6404ad1f8abff57149d5552a8cb1cec081622
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-08 10:20:57 +0700

    modified: fixed

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -6,7 +6,7 @@ const ExperienceCard = ({ number, title, text }) => {
       <h1 className="section-title text-blue-600">{number}</h1>
 
       <div>
-        <h3 className="section-title text-[32px]">{title}</h3>
+        <h3 className="section-title text-[38px]">{title}</h3>
         <p className="section-text mx-auto max-w-[220px]">{text}</p>
       </div>
     </div>

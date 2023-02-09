23d433ef48b20fab531d840bd2e86f48994c0f12
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-09 10:05:12 +0700

    modified: responsive Serivces components

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,11 +10,11 @@ const Services = () => {
   return (
     <section className="section relative">
       {/* gradient */}
-      <div className="absolute top-[500px] -left-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-yellow-300 to-green-400/90 blur-[130px]" />
+      <div className="absolute top-[500px] -left-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-yellow-300 to-green-400/90 blur-[130px] 2xl:top-[100px] 2xl:-left-[350px] 2xl:h-[600px] 2xl:w-[600px]" />
 
-      <div className="container relative grid gap-12">
+      <div className="container relative grid gap-12 sm:mx-auto sm:max-w-2xl lg:max-w-3xl 2xl:max-w-6xl">
         {/* service data */}
-        <div className="justify-self-center text-center">
+        <div className="justify-self-center text-center lg:max-w-3xl">
           <p className="section-subtitle">What we do?</p>
           <h1 className="section-title">
             We're a team of creatives who are excited about unique ideas
@@ -23,7 +23,7 @@ const Services = () => {
         </div>
 
         {/* service group */}
-        <div className="grid justify-center gap-8">
+        <div className="grid justify-center gap-8 sm:grid-cols-[repeat(2,max-content)] 2xl:grid-cols-[repeat(4,max-content)]">
           {ServiceData.map((item, index) => {
             // destructure item
             const { icon, subtitle, title, text } = item;

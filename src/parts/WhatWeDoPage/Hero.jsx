63a6dea106c24041f866d080e1d18b9ae598e6c9
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-09 20:59:20 +0700

    modified: responsive Hero - WhatWeDo page

--- src/parts/WhatWeDoPage/Hero.jsx
+++ src/parts/WhatWeDoPage/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
       {/* gradient */}
       <div className="absolute -top-[40px] -right-[150px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-pink-600 to-green-400 blur-[130px] 2xl:-bottom-[30px] 2xl:h-[600px] 2xl:w-[600px]" />
 
-      <div className="container relative grid gap-12">
-        <div className="text-center">
-          <h1 className="section-title">
+      <div className="container relative grid gap-12 sm:mx-auto sm:max-w-xl lg:max-w-5xl lg:grid-cols-2 lg:items-center 2xl:max-w-6xl">
+        <div className="text-center lg:text-left">
+          <h1 className="section-title text-[64px] lg:text-[72px]">
             We make strategies, design & development
             <span className="text-blue-600">.</span>
           </h1>
@@ -27,7 +27,11 @@ const Hero = () => {
           </Link>
         </div>
 
-        <img src={HeroImg} alt="hero img" />
+        <img
+          src={HeroImg}
+          alt="hero img"
+          className="justify-self-center 2xl:w-[520px]"
+        />
       </div>
     </section>
   );

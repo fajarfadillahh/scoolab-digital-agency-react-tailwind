e287ec6841f76a403660ca7b99209fb82445dbef
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-09 13:23:11 +0700

    modified: responsive Subscribe components

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -9,9 +9,9 @@ const Subscribe = () => {
       {/* gradient */}
       <div className="absolute top-[200px] -right-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-r from-pink-400 to-yellow-400 blur-[130px]" />
 
-      <div className="container relative grid gap-8">
+      <div className="container relative grid gap-8 xs:max-w-md sm:max-w-lg">
         {/* subscribe data */}
-        <div className="justify-self-center text-center">
+        <div className="text-center">
           <img
             src={SubscribeIcon}
             alt="subscribe icon"
@@ -22,18 +22,18 @@ const Subscribe = () => {
             Subscribe to get the latest news about us
             <span className="text-blue-600">.</span>
           </h1>
-          <p className="section-text">
+          <p className="section-text sm:mx-auto sm:max-w-md">
             We recommended you to subscribe to our newspaper, drop your email
             below to get daily update about us.
           </p>
         </div>
 
         {/* subscribe form */}
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4 xs:flex-row xs:items-center xs:rounded-full xs:border-[1.5px] xs:border-white/50 xs:p-2">
           <input
             type="email"
             placeholder="Enter email Address"
-            className="h-[55px] rounded-full border-[1.5px] border-white/50 bg-transparent text-center font-semibold -tracking-[0.9px] text-white outline-none placeholder:font-medium placeholder:text-white/50"
+            className="h-[55px] rounded-full border-[1.5px] border-white/50 bg-transparent text-center font-semibold text-white outline-none placeholder:font-medium placeholder:text-white/50 xs:w-full xs:border-none xs:pl-4 xs:text-left"
           />
           <button className="btn btn-fill" onClick={(e) => e.preventDefault()}>
             Subscribe

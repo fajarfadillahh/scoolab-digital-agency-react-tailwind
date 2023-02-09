d2ab79f439136ce90a1caebc0ffb579d1d6a3f52
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-09 10:33:22 +0700

    modified: responsive Team - HomePage

--- src/parts/HomePage/Team.jsx
+++ src/parts/HomePage/Team.jsx
@@ -13,14 +13,14 @@ const Team = () => {
   return (
     <section className="section relative">
       {/* gradient */}
-      <div className="absolute top-[500px] -left-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-r from-purple-500 to-blue-300 blur-[130px]" />
+      <div className="absolute top-[500px] -left-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-r from-purple-500 to-blue-300 blur-[130px] 2xl:top-[100px] 2xl:-left-[350px] 2xl:h-[600px] 2xl:w-[600px]" />
 
-      <div className="container relative grid gap-12">
+      <div className="container relative grid gap-12 2xl:max-w-6xl">
         {/* team data */}
         <div className="justify-self-center text-center">
           <img src={TeamIcon} alt="team icon" className="mx-auto w-20 pb-4" />
           <p className="section-subtitle">Our team</p>
-          <h1 className="section-title pb-8">
+          <h1 className="section-title">
             Meet with our
             <br />
             awesome team<span className="text-blue-600">.</span>
@@ -28,7 +28,7 @@ const Team = () => {
         </div>
 
         {/* team group */}
-        <div className="grid justify-center gap-8">
+        <div className="grid justify-center gap-8 sm:grid-cols-[repeat(2,max-content)] xl:grid-cols-[repeat(3,max-content)] 2xl:grid-cols-[repeat(4,max-content)]">
           {TeamData.map((item, index) => {
             // destructure item
             const { image, name, profession } = item;

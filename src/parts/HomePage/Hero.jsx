37f97b58dbe4e9d2822dd92ed2e3548602fca858
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-09 09:56:05 +0700

    modified: responsive Hero - HomePage

--- src/parts/HomePage/Hero.jsx
+++ src/parts/HomePage/Hero.jsx
@@ -7,14 +7,14 @@ import HeroImg1 from "../../assets/images/hero-img-1.png";
 
 const Hero = () => {
   return (
-    <section className="section relative pt-32">
+    <section className="section relative pt-32 lg:pt-40">
       {/* gradient */}
-      <div className="absolute -top-[100px] -left-[250px] h-[500px] w-[500px] rounded-full bg-gradient-to-r from-pink-600 to-yellow-200 blur-[130px]" />
-      <div className="absolute -bottom-[80px] -right-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-pink-600 to-green-400 blur-[130px]" />
+      <div className="absolute -top-[100px] -left-[250px] h-[500px] w-[500px] rounded-full bg-gradient-to-r from-pink-600 to-yellow-200 blur-[130px] 2xl:-top-[50px] 2xl:h-[600px] 2xl:w-[600px]" />
+      <div className="absolute -bottom-[80px] -right-[250px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-pink-600 to-green-400 blur-[130px] 2xl:-bottom-[30px] 2xl:h-[600px] 2xl:w-[600px]" />
 
-      <div className="container relative grid gap-12">
+      <div className="container relative grid gap-12 sm:mx-auto sm:max-w-2xl lg:max-w-4xl 2xl:max-w-6xl">
         <div className="z-10 text-center">
-          <h1 className="section-title text-[64px]">
+          <h1 className="section-title text-[64px] lg:text-[72px]">
             We're
             <br />
             <span className="relative">

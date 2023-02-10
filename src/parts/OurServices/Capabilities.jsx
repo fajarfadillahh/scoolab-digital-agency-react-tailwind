9f7bf94d33a2665dadea608ae12040b8f242f838
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-10 09:50:33 +0700

    modified: add gradient accent at Capabilities section - OurServices page

--- src/parts/OurServices/Capabilities.jsx
+++ src/parts/OurServices/Capabilities.jsx
@@ -6,7 +6,10 @@ import { CapabilitiesData } from "../../Data";
 const Capabilities = () => {
   return (
     <section className="section relative">
-      <div className="container">
+      {/* gradient */}
+      <div className="absolute -bottom-[50px] -right-[150px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-blue-500 to-purple-500 blur-[130px] 2xl:-bottom-[30px] 2xl:h-[500px] 2xl:w-[500px]" />
+
+      <div className="container relative">
         <div className="grid grid-cols-[280px] justify-center gap-8 sm:mx-auto sm:max-w-xl sm:grid-cols-2 xl:max-w-4xl xl:grid-cols-3 2xl:max-w-6xl 2xl:grid-cols-4">
           {CapabilitiesData.map((item, index) => {
             // destructure item

c9a94f845583dfdb5c41772a19c7ea91bc71fbd1
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-11 14:16:32 +0700

    modified: responsive Plans - Pricing page

--- src/parts/Pricing/Plans.jsx
+++ src/parts/Pricing/Plans.jsx
@@ -9,8 +9,8 @@ import PlanCard from "../../components/PlanCard";
 const Plans = () => {
   return (
     <section className="relative py-16">
-      <div className="container">
-        <div className="grid justify-center gap-12">
+      <div className="container 2xl:max-w-6xl">
+        <div className="grid justify-center gap-12 md:grid-cols-[repeat(2,max-content)] 2xl:grid-cols-[repeat(3,max-content)]">
           {PlanList.map((item, index) => {
             // destructure item
             const { grade, price, description, list } = item;

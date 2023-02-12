74c01333d34b23b088c0e9cf103cffab7c837f9e
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-12 21:13:06 +0700

    modified: add gradient accent at the Hero - AboutUs page

--- src/parts/AboutUsPage/Hero.jsx
+++ src/parts/AboutUsPage/Hero.jsx
@@ -6,6 +6,9 @@ import HeroImg from "../../assets/images/hero-img-4.png";
 const Hero = () => {
   return (
     <section className="section relative pt-40">
+      {/* gradients */}
+      <div className="absolute -top-[40px] -right-[150px] h-[400px] w-[400px] rounded-full bg-gradient-to-t from-pink-600 to-green-400 blur-[130px] 2xl:-bottom-[30px] 2xl:h-[600px] 2xl:w-[600px]" />
+
       <div className="container relative grid justify-items-center gap-16 2xl:max-w-6xl">
         <div className="text-center">
           <h1 className="section-title xs:text-[64px] lg:text-[72px]">
